86896afffa29eb2c8032c7d955a090f7ba5d7bf7
Danial Keoki <d.keoki@gmail.com>
2022-05-02 13:26:34 +0900

    implement article updates

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,24 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      flash[:notice] = "Article was created succesfully."
+      redirect_to article_path(@article)
+    else
+      # rails 7 : turbo - reduce page reloads
+      render :new, status: 422
+    end
+  end
+
 end
