abc996d239007954dea82bb13153c7dd73e6380c
Danial Keoki <d.keoki@gmail.com>
2022-05-04 05:36:54 +0900

    clean out code

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,16 @@
 class ArticlesController < ApplicationController
+
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+  # ----------------------------
+  public
+  # ----------------------------
+
   def show
-    @article = Article.find(params[:id])
+    begin
+    rescue Exception
+      redirect_to articles_path, status: 302
+    end
   end
 
   def index
@@ -12,11 +22,10 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     if @article.save
       flash[:notice] = "Article was created succesfully."
       redirect_to article_path(@article)
@@ -27,8 +36,7 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+    if @article.update(article_params)
       flash[:notice] = "Article was updated successfully."
       redirect_to @article
     else
@@ -38,10 +46,20 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     # turbo - reduce page reloads
-    redirect_to articles_path, status: 422
+    redirect_to articles_path, status: 302
+  end
+
+  # ----------------------------
+  private
+  # ----------------------------
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description) 
   end
 
 end
